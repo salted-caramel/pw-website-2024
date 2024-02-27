0122c386a7b07bcd66ab4ae5af28aa2a685afbac
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2024-02-27 14:41:53 +0800

    set mobile view to 1 col

--- app/components/BookLibrary.tsx
+++ app/components/BookLibrary.tsx
@@ -3,7 +3,7 @@ import BookCover from "./BookCover";
 
 const BookLibrary = () => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-4 gap-y-6 gap-x-8 max-w-4xl mx-auto">
+    <div className="grid grid-cols-1 sm:grid-cols-4 gap-y-6 gap-x-8 max-w-4xl mx-auto">
       <BookCover
         link="https://upload.wikimedia.org/wikipedia/zh/3/33/%E6%9B%B8%E5%8A%8D%E6%81%A9%E4%BB%87%E9%8C%84.jpg"
         url="/shujianenchoulu"
