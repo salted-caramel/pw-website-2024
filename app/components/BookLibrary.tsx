eb77d024a8a0e2470cb06d3ec58c0a54909444ff
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2024-02-29 14:10:32 +0800

    move data into json

--- app/components/BookLibrary.tsx
+++ app/components/BookLibrary.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 import BookCover from "./BookCover";
+import data from "../assets/data.json";
 
 const BookLibrary = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-4 gap-y-6 gap-x-8 max-w-4xl mx-auto">
       <BookCover
-        link="https://upload.wikimedia.org/wikipedia/zh/3/33/%E6%9B%B8%E5%8A%8D%E6%81%A9%E4%BB%87%E9%8C%84.jpg"
+        link={data[0].url}
         url="/shujianenchoulu"
-        text="书剑恩仇录 "
+        text={data[0].title}
       />
       <BookCover
         link="https://upload.wikimedia.org/wikipedia/zh/5/52/%E7%A2%A7%E8%A1%80%E5%8A%8D.jpg"
         url="/bixuejian"
         text="碧血剑"
-      />
-      <BookCover
-        link="https://upload.wikimedia.org/wikipedia/zh/3/33/%E6%9B%B8%E5%8A%8D%E6%81%A9%E4%BB%87%E9%8C%84.jpg"
-        url="/shujianenchoulu"
-        text="test"
       />
     </div>
   );
