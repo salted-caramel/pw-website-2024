3115951acd80bdfeea255ae5b965a6a0996fcef0
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2024-03-01 13:49:49 +0800

    change link and added tooltip

--- app/components/BookIntroPage.tsx
+++ app/components/BookIntroPage.tsx
@@ -19,7 +19,7 @@ const BookIntroPage = ({ title, url, stars, intro, readLink }: Props) => {
       <div className="flex sm:flex-row flex-col gap-x-36 gap-y-20">
         <div className="flex flex-col gap-y-4 min-w-72">
           <div className="flex flex-row items-center">
-            <a href="/">
+            <a href="/" className="tooltip" data-tip="返回主页">
               <FaArrowLeft className="h-6 w-6 mr-4 hover:text-slate-500 transition duration-300 ease-in-out" />
             </a>
             <p className="text-2xl font-bold">{title}</p>
