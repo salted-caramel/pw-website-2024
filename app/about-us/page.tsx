1d2ac3794a141a65594dd3a7a83f3879ef17c440
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2024-03-09 17:08:33 +0800

    add robots.txt

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -4,6 +4,7 @@ import Nav from "../components/Nav";
 import PersonCard from "../components/PersonCard";
 
 import jerryImg from "../../public/images/jerry.jpg";
+import jwImg from "../../public/images/jw.jpg";
 
 const aboutus = () => {
   return (
@@ -12,7 +13,7 @@ const aboutus = () => {
       <div>
         <p className="text-3xl">我们的组员</p>
         <div className="flex sm:flex-row flex-col gap-6 sm:justify-center align-center">
-          <PersonCard name="李俊緯" image={jerryImg} isLeader={true} />
+          <PersonCard name="李俊緯" image={jwImg} isLeader={true} />
           <PersonCard name="刘宗翰" image={jerryImg} />
           <PersonCard name="周睿宇" image={jerryImg} />
           <PersonCard name="李熙源" image={jerryImg} />
