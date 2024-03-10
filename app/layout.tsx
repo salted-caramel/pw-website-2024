2c84b90323db93e115556f14b7ff76ddb05e66f4
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2024-03-10 10:37:25 +0800

    add meta tags and update robots

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "叱咤武林",
-  description: "武侠小说介绍",
+  title: "叱咤武林 - 金庸武侠小说介绍",
+  description: "金庸武侠小说介绍",
 };
 
 export default function RootLayout({
@@ -21,6 +21,8 @@ export default function RootLayout({
           name="google-site-verification"
           content="RLU1BTgCMIs6Q9VnSVZ7hnasZ1gbyQ0wIV972SRdx_k"
         />
+        <meta name="robots" content="all" />
+        <meta name="description" content="金庸武侠小说介绍" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
