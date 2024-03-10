ec56afd39a0e1e81e7d5055ed7318eab4588a1f4
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2024-03-10 09:11:18 +0800

    change to blank pfp

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -5,6 +5,7 @@ import PersonCard from "../components/PersonCard";
 
 import jerryImg from "../../public/images/jerry.jpg";
 import jwImg from "../../public/images/jw.jpg";
+import blankImg from "../../public/images/blankuser.png";
 
 const aboutus = () => {
   return (
@@ -15,8 +16,8 @@ const aboutus = () => {
         <div className="flex sm:flex-row flex-col gap-6 sm:justify-center align-center">
           <PersonCard name="李俊緯" image={jwImg} isLeader={true} />
           <PersonCard name="刘宗翰" image={jerryImg} />
-          <PersonCard name="周睿宇" image={jerryImg} />
-          <PersonCard name="李熙源" image={jerryImg} />
+          <PersonCard name="周睿宇" image={blankImg} />
+          <PersonCard name="李熙源" image={blankImg} />
         </div>
       </div>
     </div>
