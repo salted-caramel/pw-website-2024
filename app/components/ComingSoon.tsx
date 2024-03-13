7fc1734c77295022237a5b8bdca69507b9159f36
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2024-03-13 20:34:41 +0800

    fix typo

--- app/components/ComingSoon.tsx
+++ app/components/ComingSoon.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ComingSoon() {
   return (
     <div className="flex justify-center items-center h-screen flex-col">
-      <h1 className="text-7xl text-center font-bold">尽情期待</h1>
+      <h1 className="text-7xl text-center font-bold">敬请期待</h1>
       <a href="/" className="text-4xl mt-10">
         返回主页
       </a>
