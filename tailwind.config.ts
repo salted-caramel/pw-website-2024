9913d55cb033de45936f6a5a10d70e8b2ce5073e
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2024-03-18 16:16:14 +0800

    revert

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,6 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        main: "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQFmXv9Rh3UbpKnbXdLmiglCAoeSTfJd2VjZRs9laDcA&s)",
       },
     },
   },
