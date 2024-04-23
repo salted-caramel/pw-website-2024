634cd84ed61401cadeacda15984f5d799b3c9c4b
salted-caramel <105400878+salted-caramel@users.noreply.github.com>
2024-04-23 09:16:20 +0800

    update timeline

--- app/components/Timeline.tsx
+++ app/components/Timeline.tsx
@@ -19,18 +19,10 @@ const Timeline = () => {
           </svg>
         </div>
         <div className="timeline-start md:text-end mb-10">
-          <time className="font-mono italic text-black">1984</time>
-          <div className="text-lg font-black text-black">
-            First Macintosh computer
-          </div>
-          <p className="text-black">
-            The Apple Macintosh—later rebranded as the Macintosh 128K—is the
-            original Apple Macintosh personal computer. It played a pivotal role
-            in establishing desktop publishing as a general office function. The
-            motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were
-            housed in a beige case with integrated carrying handle; it came with
-            a keyboard and single-button mouse.
-          </p>
+          <time className="font-mono italic text-black">东周</time>
+          <div className="text-lg font-black text-black">《越女剑》</div>
+          {/* <p className="text-black">
+          </p> */}
         </div>
         <hr />
       </li>
@@ -51,14 +43,10 @@ const Timeline = () => {
           </svg>
         </div>
         <div className="timeline-end mb-10">
-          <time className="font-mono italic text-black">1998</time>
-          <div className="text-lg font-black text-black">iMac</div>
-          <p className="text-black">
-            iMac is a family of all-in-one Mac desktop computers designed and
-            built by Apple Inc. It has been the primary part of Apples consumer
-            desktop offerings since its debut in August 1998, and has evolved
-            through seven distinct forms
-          </p>
+          <time className="font-mono italic text-black">北宋</time>
+          <div className="text-lg font-black text-black">《天龙八部》</div>
+          {/* <p className="text-black">
+          </p> */}
         </div>
         <hr />
       </li>
@@ -79,17 +67,10 @@ const Timeline = () => {
           </svg>
         </div>
         <div className="timeline-start md:text-end mb-10">
-          <time className="font-mono italic text-black">2001</time>
-          <div className="text-lg font-black text-black">iPod</div>
-          <p className="text-black">
-            The iPod is a discontinued series of portable media players and
-            multi-purpose mobile devices designed and marketed by Apple Inc. The
-            first version was released on October 23, 2001, about 8+1⁄2 months
-            after the Macintosh version of iTunes was released. Apple sold an
-            estimated 450 million iPod products as of 2022. Apple discontinued
-            the iPod product line on May 10, 2022. At over 20 years, the iPod
-            brand is the oldest to be discontinued by Apple
-          </p>
+          <time className="font-mono italic text-black">南宋</time>
+          <div className="text-lg font-black text-black">《射雕英雄传》</div>
+          {/* <p className="text-black">
+          </p> */}
         </div>
         <hr />
       </li>
@@ -110,17 +91,10 @@ const Timeline = () => {
           </svg>
         </div>
         <div className="timeline-end mb-10">
-          <time className="font-mono italic text-black">2007</time>
-          <div className="text-lg font-black text-black">iPhone</div>
-          <p className="text-black">
-            iPhone is a line of smartphones produced by Apple Inc. that use
-            Apples own iOS mobile operating system. The first-generation iPhone
-            was announced by then-Apple CEO Steve Jobs on January 9, 2007. Since
-            then, Apple has annually released new iPhone models and iOS updates.
-            As of November 1, 2018, more than 2.2 billion iPhones had been sold.
-            As of 2022, the iPhone accounts for 15.6% of global smartphone
-            market share
-          </p>
+          <time className="font-mono italic text-black">宋末元初</time>
+          <div className="text-lg font-black text-black">《神雕侠侣》</div>
+          {/* <p className="text-black">
+          </p> */}
         </div>
         <hr />
       </li>
@@ -141,14 +115,202 @@ const Timeline = () => {
           </svg>
         </div>
         <div className="timeline-start md:text-end mb-10">
-          <time className="font-mono italic text-black">2015</time>
-          <div className="text-lg font-black text-black">Apple Watch</div>
-          <p className="text-black">
-            The Apple Watch is a line of smartwatches produced by Apple Inc. It
-            incorporates fitness tracking, health-oriented capabilities, and
-            wireless telecommunication, and integrates with iOS and other Apple
-            products and services
-          </p>
+          <time className="font-mono italic text-black">元末明初</time>
+          <div className="text-lg font-black text-black">《倚天屠龙记》</div>
+          {/* <p className="text-black">
+          </p> */}
+        </div>
+        <hr />
+      </li>
+      <li>
+        <hr />
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="black"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-end mb-10">
+          <time className="font-mono italic text-black">明</time>
+          <div className="text-lg font-black text-black">《笑傲江湖》</div>
+          {/* <p className="text-black">
+          </p> */}
+        </div>
+        <hr />
+      </li>
+      <li>
+        <hr />
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="black"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-start md:text-end mb-10">
+          <time className="font-mono italic text-black">明末清初</time>
+          <div className="text-lg font-black text-black">《碧血剑》</div>
+          {/* <p className="text-black">
+          </p> */}
+        </div>
+        <hr />
+      </li>
+      <li>
+        <hr />
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="black"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-end mb-10">
+          <time className="font-mono italic text-black">清初</time>
+          <div className="text-lg font-black text-black">《鸳鸯刀》</div>
+          {/* <p className="text-black">
+          </p> */}
+        </div>
+        <hr />
+      </li>
+      <li>
+        <hr />
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="black"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-start md:text-end mb-10">
+          <time className="font-mono italic text-black">清</time>
+          <div className="text-lg font-black text-black">《白马啸西风》</div>
+          {/* <p className="text-black">
+          </p> */}
+        </div>
+        <hr />
+      </li>
+      <li>
+        <hr />
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="black"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-end mb-10">
+          <time className="font-mono italic text-black">清 康熙</time>
+          <div className="text-lg font-black text-black">《鹿鼎记》</div>
+          {/* <p className="text-black">
+          </p> */}
+        </div>
+        <hr />
+      </li>
+      <li>
+        <hr />
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="black"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-start md:text-end mb-10">
+          <time className="font-mono italic text-black">清 乾隆</time>
+          <div className="text-lg font-black text-black">《书剑恩仇录》</div>
+          {/* <p className="text-black">
+          </p> */}
+        </div>
+        <hr />
+      </li>
+      <li>
+        <hr />
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="black"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-end mb-10">
+          <time className="font-mono italic text-black">清 乾隆</time>
+          <div className="text-lg font-black text-black">《飞狐外传》</div>
+          {/* <p className="text-black">
+          </p> */}
+        </div>
+        <hr />
+      </li>
+      <li>
+        <hr />
+        <div className="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="black"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="timeline-start md:text-end mb-10">
+          <time className="font-mono italic text-black">清 乾隆</time>
+          <div className="text-lg font-black text-black">《雪山飞狐》</div>
+          {/* <p className="text-black">
+          </p> */}
         </div>
       </li>
     </ul>
